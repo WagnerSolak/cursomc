f297d355c5818870f970b45dfee8519f667b1daf
Wagner Solak <wwwagnerww@gmail.com>
2018-10-14 15:37:11 -0300

    Banco de Dados H2 ok

--- src/main/java/com/solak/cursomc/domain/Categoria.java
+++ src/main/java/com/solak/cursomc/domain/Categoria.java
@@ -2,12 +2,22 @@ package com.solak.cursomc.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+
+@Entity
 public class Categoria implements Serializable{
 	
 	//interface que fala que o objeto pode ser trafegar em rede por exemplo
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
+	
 	private String nome;
 	
 	public Categoria() {
