3f11ea3e1b6d0e55c1a4c04d0714cef42c41bfdb
Wagner Solak <wwwagnerww@gmail.com>
2018-10-15 18:43:14 -0300

    Serializacao Cíclica OK e Endpoint/clientes{id}disponivel

---
+++ src/main/java/com/solak/cursomc/resources/ClienteResource.java
@@ -0,0 +1,29 @@
+package com.solak.cursomc.resources;
+
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.solak.cursomc.domain.Cliente;
+import com.solak.cursomc.services.ClienteService;
+
+@RestController
+@RequestMapping(value="/clientes")  //endpoint
+public class ClienteResource {
+	
+	@Autowired
+	private ClienteService service;
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.GET) //requisicao para obter dados
+	public ResponseEntity<?> find(@PathVariable Integer id) {
+		
+		Cliente obj = service.buscar(id);
+		return ResponseEntity.ok().body(obj);
+		
+	}
+}
---
+++ src/main/java/com/solak/cursomc/services/ClienteService.java
@@ -0,0 +1,24 @@
+package com.solak.cursomc.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.solak.cursomc.domain.Cliente;
+import com.solak.cursomc.repositories.ClienteRepository;
+import com.solak.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+	
+	@Autowired      
+	private ClienteRepository repo;
+	
+	public Cliente buscar(Integer id) {
+		Cliente obj = repo.findOne(id);
+		if(obj == null) {
+			throw new ObjectNotFoundException("Objeto não encontrado! ID:" +id
+					+ ", Tipo: " + Cliente.class.getName());
+		}
+		return obj;
+	}
+}
